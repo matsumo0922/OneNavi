16e63dc0c04913311e9bc55c065f71964a845eca
matsumo0922 <caios.system@gmail.com>
2026-06-10 23:35:38 +0900

    refactor: remove Android Auto host hook path

--- core/common/src/androidMain/kotlin/me/matsumo/onenavi/core/common/car/CarDisplayState.kt
+++ core/common/src/androidMain/kotlin/me/matsumo/onenavi/core/common/car/CarDisplayState.kt
@@ -5,8 +5,8 @@ import java.util.concurrent.atomic.AtomicBoolean
 /**
  * 車載ディスプレイ(Android Auto)上で OneNavi が表示中かをプロセス全体で共有するフラグ。
  *
- * CarActivity のライフサイクルで更新し、案内音声の出力チャンネル選択などプロセス内 singleton から
- * 参照する。スマホ側 (MainActivity) からは更新されないため、車載中のみ true となる。
+ * Android Auto の template screen ライフサイクルで更新し、案内音声の出力チャンネル選択など
+ * プロセス内 singleton から参照する。スマホ側 (MainActivity) からは更新されないため、車載中のみ true となる。
  */
 object CarDisplayState {
 
