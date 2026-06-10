d94ef1bdaa900c9a3b071efc84720d3a519011de
matsumo0922 <caios.system@gmail.com>
2026-06-11 01:28:59 +0900

    fix: consume phone search command before expansion

--- feature/map/src/androidMain/kotlin/me/matsumo/onenavi/feature/map/components/topappbar/MapTopAppBar.kt
+++ feature/map/src/androidMain/kotlin/me/matsumo/onenavi/feature/map/components/topappbar/MapTopAppBar.kt
@@ -165,6 +165,7 @@ internal fun MapTopAppBar(
     LaunchedEffect(destinationSearchRequestId) {
         val requestId = destinationSearchRequestId ?: return@LaunchedEffect
 
+        onDestinationSearchRequestConsumed(requestId)
         canFocus = true
         showSearchResult = false
         searchBarState.animateToExpanded()
@@ -173,7 +174,6 @@ internal fun MapTopAppBar(
             destinationSearchFocusRequester.requestFocus()
         }
         softwareKeyboardController?.show()
-        onDestinationSearchRequestConsumed(requestId)
     }
 
     Box(
